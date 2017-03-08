cc998ca9c6da92c90fcf21c40d2c712e45e3d1a2
AngryShrimp <johnstone.keenan@gmail.com>
2017-03-07 19:25:20 -0600

    Assign3 work

--- Assingment2/problem1.scala
+++ Assingment2/problem1.scala
@@ -1,8 +1,17 @@
+
+/*
+	Pre conditions: Vec1 is of the same length as vec2
+	Post Conditions: The returned value is not negative
+*/
 def vecSumSquareDiff(vec1:Vector[Double], vec2:Vector[Double]) = {
 	require(vec1.length == vec2.length);
 	vec1.zip(vec2).map(v => pow(abs(v._1 - v._2), 2)).sum
 }.ensuring(ret => ret >= 0)
 
+/*
+	Pre conditions: seq has 2 or more elements, the index is not greater than the length of the seq
+	Post Conditions: The "furthest" value is the furthest value from the index
+*/
 def indexForFurthestElt(seq:Seq[Double], i:Int) = {
     require(seq.length >= 2);
     require(i >= 0 && i < seq.length);
